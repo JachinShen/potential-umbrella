03f4a9458be462ada2b8ac853882594103555a51
JachinShen <jachinshen@foxmail.com>
2020-02-10 18:54:00 +0800

    add comments on BatchGroup

--- group.py
+++ group.py
@@ -52,44 +52,55 @@ class Group(object):
 
 
 class GroupBatch():
+    """Class to represent batch of groups.
+
+    Computer many groups effectively.
+
+    Attributes:
+        orth_terms: Terms to make the permutation orthogonal.
+        n_expr: Half of the number of expressions in one group.
+        standard: A torch tensor containing the outputs of permutation, used to test permutation.
+        batch_size: Number of groups.
+        list_grps: List of groups.
     """
-    GroupBatch
-    """
-    pair_base_terms = ["x4", "x5", "x6", "x7"]
-    n_expr = len(pair_base_terms)
+    orth_terms = ["x4", "x5", "x6", "x7"]
+    n_expr = len(orth_terms)
     standard = torch.arange(ep.N_INPUT_X, dtype=torch.int64)
 
     def __init__(self, list_grps):
-        """Init"""
         if len(list_grps) == 0:
             print("No expression in the group!")
             return
         self.batch_size = len(list_grps)
-        self.list_grps = list(map(self.preprocess_group, list_grps))
+        self.list_grps = list(map(self.__preprocess_group, list_grps))
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.standard = self.standard.to(device)
 
-    def preprocess_group(self, list_exprs):
-        """Preprocess"""
+    def __preprocess_group(self, list_exprs):
         if isinstance(list_exprs[0], str):
             list_exprs = list(map(ep.Expr, list_exprs))
-        for expr, p_term in zip(list_exprs[::-1], self.pair_base_terms):
+        for expr, p_term in zip(list_exprs[::-1], self.orth_terms):
             list_exprs.append(expr.get_pair_expr() + p_term)
         return list_exprs
 
     def run(self):
-        """Run"""
+        """Get the output of all groups
+        """
+        # View groups as many expressions.
         list_exprs = list(itertools.chain(*self.list_grps))
         res = ep.ExprBatch(list_exprs).run()
         res = res.reshape(self.batch_size, 2*self.n_expr, -1).long()
         return res
 
     def test_permutation(self):
-        """Test"""
-        list_exprs = list(itertools.chain(*self.list_grps))
-        res = ep.ExprBatch(list_exprs).run()
-        res = res.reshape(self.batch_size, 2*self.n_expr, -1).long()
-        res = sum([res[:, i, :] << i for i in range(ep.LEN_ALPHA-2)])
+        """Test permutation of each group
+        """
+        res = self.run()
+        # Convert outputs (bits) to scales.
+        res = sum([res[:, i, :] << i for i in range(ep.N_X)])
+        # The group is permutation iff the outputs cover the whole domain [0, 2^N_X].
+        # Sort each group for convenience to compare with **standard**.
+        # **standard** is the outputs [0, 2^N_X]
         res = res.sort(dim=1)[0]
         is_perm = (res == self.standard).all(dim=1)
         return is_perm
